e2658c63469bbd38b7ab659c61243976bcd937a1
sugar1569 <sugar1569@vip.qq.com>
2022-06-30 15:06:25 +0800

    【程序目录】修改配置文件

--- crmeb/config/upload.php
+++ crmeb/config/upload.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 
 return [
-    //默认上传模式
+    //默认上传模式,后台配置优先,添加类型一定索引要和驱动名一致 用小些字母
     'default' => 'local',
     //上传文件大小
     'filesize' => 2097152,
@@ -35,15 +35,25 @@ return [
         'image/x-icon',
         'image/vnd.microsoft.icon',
     ],
-    //驱动模式
+    //驱动模式，此配置优先与后台配置，后台添加配置请加前缀，例如添加七牛云配置：accessKey 后台添加变量名 qiniu_accessKey
     'stores' => [
         //本地上传配置
         'local' => [],
         //七牛云上传配置
-        'qiniu' => [],
+        'qiniu' => [
+            'accessKey' => '', // sys_config('qiniu_accessKey')
+            'secretKey' => '', // sys_config('qiniu_secretKey')
+        ],
         //oss上传配置
-        'oss' => [],
+        'oss' => [
+            'accessKey' => '', // sys_config('accessKey')
+            'secretKey' => '', // sys_config('secretKey')
+        ],
         //cos上传配置
-        'cos' => [],
+        'cos' => [
+            'accessKey' => '', //sys_config('tengxun_accessKey')
+            'secretKey' => '', //sys_config('tengxun_secretKey')
+            'appid' => '', //sys_config('tengxun_appid')
+        ],
     ]
 ];
